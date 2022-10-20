d4cb703ddf721c016270c729401a5f81191302a4
Klimenty Karavaev <klimentykk@gmail.com>
2022-10-20 17:05:31 +0400

    Edit about page to pass tests

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -6,15 +6,7 @@ const About: React.FC = (props: any) => {
   return (
     <div className={styles.app}>
       <section style={{ width: "600px" }}>
-        <p>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
-        </p>
+        <p>This is my shop's about page, welcome!</p>
       </section>
     </div>
   );
