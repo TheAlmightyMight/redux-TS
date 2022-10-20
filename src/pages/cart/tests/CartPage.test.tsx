766f09abaa981db72468afa05a00c72457696a70
Klimenty Karavaev <klimentykk@gmail.com>
2022-10-20 19:22:57 +0400

    Fix crashing bug with singleProduct page on reload

---
+++ src/pages/cart/tests/CartPage.test.tsx
@@ -0,0 +1,2 @@
+import { screen, render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
--- src/pages/product/SingleProducts.tsx
+++ src/pages/product/SingleProducts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useAppDispatch, useAppSelector } from "../../types/redux-hooks";
 //@ts-ignore
@@ -7,27 +7,43 @@ import { addCartItemAsync } from "../../redux/actionCreators/CartActions";
 
 //types
 import { ProductItem } from "../../types/ProductItem";
+import { CartItem } from "../../types/CartItem";
+
+//crashes on page reload
 
 function SingleProducts() {
   const { id } = useParams();
+
   const dispatch = useAppDispatch();
-  const item = useAppSelector((state) =>
-    state.productReducer.products.find((el: ProductItem) => el.id === id)
-  ) as ProductItem;
   const logged = useAppSelector((state) => state.authReducer.isLogged);
-  const { id: _, ...rest } = item;
-  const itemToPassAsArgument = { ...rest, quantity: 1 };
+
+  const [item, setItem] = useState<ProductItem>();
+  const [itemProps, setItemProps] = useState<Omit<CartItem, "id">>();
+
+  useEffect(() => {
+    fetch(`https://panicky-swimsuit-tuna.cyclic.app/products/${id}`)
+      .then((res) => res.json())
+      .then((res) => setItem(res))
+      .then(() => {
+        const { id: _, ...rest } = item as ProductItem;
+        const itemToPassAsArgument = { ...rest, quantity: 1 };
+        console.log(item);
+
+        setItemProps(itemToPassAsArgument);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <article className={styles.innerContainer}>
-        <h3 style={{ margin: "0", padding: "0" }}>{item.title}</h3>
+        <h3 style={{ margin: "0", padding: "0" }}>{item?.title}</h3>
         <figure className={styles.imgContainer}>
           <img
             className={styles.image}
-            alt={`Товар ${item.id} картинка отвалилась`}
-            src={item.picture}
+            alt={`Товар ${item?.id} картинка отвалилась`}
+            src={item?.picture}
           />
-          <figcaption>Описание:{item.description}</figcaption>
+          <figcaption>Описание:{item?.description}</figcaption>
         </figure>
         {logged ? (
           <button
@@ -39,7 +55,9 @@ function SingleProducts() {
               borderRadius: "15px",
               cursor: "pointer",
             }}
-            onClick={() => dispatch(addCartItemAsync(itemToPassAsArgument))}
+            onClick={() =>
+              dispatch(addCartItemAsync(itemProps as Omit<CartItem, "id">))
+            }
           >
             Add
           </button>
